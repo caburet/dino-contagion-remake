6134cccf33f13757f7952759beac063cf5056183
Joel Partida <joelp@vianet.com.ar>
2020-01-30 17:53:55 -0300

    Arreglos

---
+++ Scripts/global.gd
@@ -0,0 +1,13 @@
+extends Node
+
+const COLUMNS = 10
+const ROWS = 8
+const bloqueables = [3, 4, 5, 6, 8, 9, 32, 33]
+
+var dinos_fixed = {}
+var modo_play = false
+var level_selected = 1
+var reload=false
+
+func _ready():	
+	pass
